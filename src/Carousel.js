c2dd4ee856d4f45b28e16dbf544650db244c36e4
jxuxaxcxo <joaquin.vargaselio@gmail.com>
2018-05-28 01:54:08 -0400

    Navbar

---
+++ src/Carousel.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import logo from './logo.png';
+
+
+
+class Carousel extends Component {
+  render() {
+    return (
+       
+        <div className="NavBar">
+        </div>
+    );
+  }
+}
+
+export default Carousel;
--- src/index.js
+++ src/index.js
@@ -4,5 +4,12 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap/dist/css/bootstrap-theme.css';
+import NavBar from './NavBar';
+
+
+
+ReactDOM.render(<NavBar />, document.getElementById('root'));
 registerServiceWorker();
